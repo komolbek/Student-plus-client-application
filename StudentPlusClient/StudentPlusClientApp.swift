872d8e6422e9c95a4f3c8b2dba1d276d27959b2f
komolibragimov <rusmirolub@gmail.com>
2023-01-13 14:06:48 +0000

    initial commit

---
+++ StudentPlusClient/StudentPlusClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentPlusClientApp.swift
+//  StudentPlusClient
+//
+//  Created by Komol Ibragimov on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudentPlusClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
